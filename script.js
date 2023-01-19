0a5bff34d9756ac9b4213192d95733b04b486dd1
omatsola619 <omarsunday0@gmail.com>
2023-01-19 11:41:51 +0100

    adjusted the menu to fit screen width and used a different menu for the mobile view

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector(".hamburger-div");
-const listItems = document.querySelector(".unordered-list");
+const mobileMenu = document.querySelector(".mobile-unordered-list");
+let hamburgerIcon = document.querySelector(".bi bi-list")
+
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
-  listItems.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+  hamburgerIcon.classList.toggle("bi bi-x-lg");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   listItems.classList.remove("active");
 }))
+
